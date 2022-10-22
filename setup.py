3b449337598b6efd24a23f506bdabbc3484544ff
Holden Rohrer <hr@hrhr.dev>
2022-10-22 02:23:35 -0400

    added setuptools

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='shabinets',
+    version='0.0',
+    description='Not so shabby food inventory management',
+    author='Sean Aoki, Nithya Jayakumar, Vivien Orellana, Holden Rohrer',
+    packages=['shabinets'],
+    install_requires=[],
+)
