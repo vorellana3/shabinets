35902b09064912584ded2db6d336dc76487b75f5
Vivien <vorellana3@gatech.edu>
2022-10-22 18:25:37 -0400

    added sql skeleton

---
+++ shabinets/shabinets_database.sql
@@ -0,0 +1,31 @@
+drop database if exists shabinets;
+create database if not exists shabinets;
+use shabinets;
+
+create table food (
+	id decimal(9, 0) not null,
+    food_name char(10) not null,
+    units char(10) not null,
+    primary key (id)
+) engine = innodb;
+
+create table recipes (
+	id decimal(9, 0) not null,
+    recipe_name char(10) not null,
+    primary key (id)
+) engine = innodb;
+
+create table recipe_ingredient (
+	food_id decimal(9,0) not null,
+    recipe_id decimal(9, 0) not null,
+    amount decimal(9,0) not null,
+    primary key (food_id, recipe_id)
+) engine = innodb;
+
+create table user_food (
+	food_id decimal(9,0) not null,
+    bought_date date not null,
+    expire_date date,
+    amount decimal(9,0) not null,
+    primary key (food_id)
+) engine = innodb;
