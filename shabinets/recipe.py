c895d0dff660fa8b01a16c6c590c5b43d36f9a95
Vivien <vorellana3@gatech.edu>
2022-10-23 00:55:38 -0400

    Added Recipe Class

--- shabinets/recipe.py
+++ shabinets/recipe.py
@@ -1,9 +1,12 @@
+from http.client import PROXY_AUTHENTICATION_REQUIRED
 import json
 import datetime
+from unicodedata import name
 import food
 from time import sleep
 import random
 import requests
+import json
 import food
 import DataHandler
 
@@ -26,7 +29,6 @@ def getAllRecipes():
         response = json.dumps(response.json(), indent=4)
         with open("recipes.json", "a") as outfile:
             outfile.write(response)
-
 def getNextRecipe():
     #access next perishable to expire
     database = DataHandler()
@@ -34,10 +36,30 @@ def getNextRecipe():
     in_database = False
     #if it's in the database, get recipe here
     if in_database:
-        
+        print ("PLACEHOLDER")
     else:
         print("NAME: " + next_perishable.name)
         return getRecipe(next_perishable.name)
     
+class Recipe:
+    name = None
+    picture = None
+    id = None
+    ingredients = []
 
+    def __init__(self, name, picture, id, ingredients):
+        self.name = name
+        self.picture = picture
+        self.id = id
+        self.ingredients = ingredients
+
+    def getJson(self) :
+        value = {
+            "recipe": {
+                "label": self.name,
+                "ingrdientlines": self.ingredients,
+                "url": self.picture
+            }
+        }
+        return json.dumps(value)
 
